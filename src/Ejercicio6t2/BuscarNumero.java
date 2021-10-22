bf793db3246d427602cbe248c3a30a3a2b5b831e
Moises <moisessepseg@gmail.com>
2021-10-22 10:00:24 +0200

    Ejercicio 6

---
+++ src/Ejercicio6t2/BuscarNumero.java
@@ -0,0 +1,52 @@
+package Ejercicio6t2;
+
+//Programa creado por moises Sepulveda Segarra
+
+//Clase para buscar numero
+public class BuscarNumero implements Runnable {
+	
+	private int num;
+	
+	//Constructor
+	public  BuscarNumero (int num) {
+		this.num=num;
+	}
+	
+	//Ahora buscamos los numeros
+	@Override
+	public void run() {
+		int num2 = num;
+		if (num2 == 1) {
+		}
+		
+		else if (num2 == 2) {
+			
+		}
+		else if (num2 == 3) {
+			
+		}
+		else if (num2 == 4) {
+	
+		}
+		else if (num2 == 5) {
+	
+		}
+		else if (num2 == 6) {
+	
+		}
+		else if (num2 == 7) {
+	
+		}
+		else if (num2 == 8) {
+	
+		}
+		else if (num2 == 9) {
+	
+		}
+		else if (num2 == 2) {
+	
+		}
+		
+	}
+
+}
